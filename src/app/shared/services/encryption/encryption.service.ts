beff5b4985fc9474f33847c6dc59630652a2fc72
basel12345 <baslelsawy@gmail.com>
2024-10-12 18:13:46 +0300

    remove build

--- src/app/shared/services/encryption/encryption.service.ts
+++ src/app/shared/services/encryption/encryption.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as CryptoJS from 'crypto-js';
+// import * as CryptoJS from 'crypto-js';
 
 @Injectable({
 	providedIn: 'root'
@@ -8,18 +8,18 @@ export class EncryptionService {
 	constructor() { }
 
 
-	public encryptData(data: string): string {
-		let key = CryptoJS.enc.Utf8.parse('13313586896631234900207000800912');
-		let iv = CryptoJS.enc.Utf8.parse('6896631234900212');
-		var encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(data), key,
-			{
-				keySize: 128 / 8,
-				iv: iv,
-				mode: CryptoJS.mode.CBC,
-				padding: CryptoJS.pad.Pkcs7
-			}).toString();
-		return encrypted;
-	}
+	// public encryptData(data: string): string {
+	// 	let key = CryptoJS.enc.Utf8.parse('13313586896631234900207000800912');
+	// 	let iv = CryptoJS.enc.Utf8.parse('6896631234900212');
+	// 	var encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(data), key,
+	// 		{
+	// 			keySize: 128 / 8,
+	// 			iv: iv,
+	// 			mode: CryptoJS.mode.CBC,
+	// 			padding: CryptoJS.pad.Pkcs7
+	// 		}).toString();
+	// 	return encrypted;
+	// }
 
 
 }
